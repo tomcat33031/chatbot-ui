c14614cb53602ea0dd88492557f7aadf4b4082f9
Andy Nguyen <andynguyen2410@outlook.com>
2023-05-06 17:26:26 +0700

    Remove redundant text in HomePage

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -352,17 +352,17 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       {!(apiKey || serverSideApiKeyIsSet) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
-            Welcome to Chatbot UI
+            Welcome to ChatGPTiny
           </div>
           <div className="text-center text-lg text-black dark:text-white">
-            <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
-            <div className="mb-2 font-bold">
-              Important: Chatbot UI is 100% unaffiliated with OpenAI.
-            </div>
+            {/* <div className="mb-8">{`ChatGPTiny is an open source clone of OpenAI's ChatGPT UI.`}</div> */}
+            {/* <div className="mb-2 font-bold">
+              Important: ChatGPTiny is 100% unaffiliated with OpenAI.
+            </div> */}
           </div>
           <div className="text-center text-gray-500 dark:text-gray-400">
             <div className="mb-2">
-              Chatbot UI allows you to plug in your API key to use this UI with
+              ChatGPTiny allows you to plug in your API key to use this UI with
               their API.
             </div>
             <div className="mb-2">
@@ -386,6 +386,9 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
               </a>
             </div>
           </div>
+          <div className="text-center">
+              Please contact me for work: <a href="mailto:andynguyen2410@outlook.com" target="_blank" rel="noreferrer" class="text-blue-500 hover:underline">andynguyen2410@outlook.com</a>.
+          </div>
         </div>
       ) : modelError ? (
         <ErrorMessageDiv error={modelError} />
@@ -405,7 +408,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                         <Spinner size="16px" className="mx-auto" />
                       </div>
                     ) : (
-                      'Chatbot UI'
+                      'ChatGPTiny'
                     )}
                   </div>
 
